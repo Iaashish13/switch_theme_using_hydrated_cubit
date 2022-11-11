c7a937ff554170f763d38952d3bc4376b295591e
Aashish Regmi <jungali1000@gmail.com>
2022-11-11 16:19:02 +0545

    gif added

--- lib/theme/light_them.dart
+++ lib/theme/light_them.dart
@@ -6,15 +6,14 @@ final lightTheme = ThemeData(
   primaryColor: Colors.blueAccent,
   scaffoldBackgroundColor: Colors.white,
   textTheme: const TextTheme(
-    bodyText1: TextStyle(
-      fontWeight: FontWeight.w500,
-      fontSize: 14,
-      color: Colors.black,
-    ),
-     bodyText2: TextStyle(
-      fontWeight: FontWeight.w300,
-      fontSize: 11,
-      color: Colors.grey,
-    )
-  ),
+      bodyText1: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 16,
+        color: Colors.black,
+      ),
+      bodyText2: TextStyle(
+        fontWeight: FontWeight.w300,
+        fontSize: 13,
+        color: Colors.grey,
+      )),
 );
