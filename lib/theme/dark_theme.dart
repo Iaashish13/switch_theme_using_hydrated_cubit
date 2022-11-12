fb0a6bef861eb4d2ce0baf4be1697eaa2f06311b
Aashish Regmi <jungali1000@gmail.com>
2022-11-12 12:37:14 +0545

    gif updated

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -8,12 +8,12 @@ final darkTheme = ThemeData(
   textTheme: const TextTheme(
     bodyText1: TextStyle(
       fontWeight: FontWeight.w500,
-      fontSize: 14,
+      fontSize: 16,
       color: Colors.white,
     ),
     bodyText2: TextStyle(
       fontWeight: FontWeight.w300,
-      fontSize: 11,
+      fontSize: 13,
       color: Colors.grey,
     ),
   ),
